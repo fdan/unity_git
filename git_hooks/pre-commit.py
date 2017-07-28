f2fd9d246d157d133e25053e8feeae305cfdcd4f
Daniel Flood <daniel.flood.80@gmail.com>
2017-07-29 00:44:37 +1000

    fix pre-commit.py

--- git_hooks/pre-commit.py
+++ git_hooks/pre-commit.py
@@ -4,71 +4,121 @@ import subprocess
 from subprocess import Popen, PIPE
 import re
 import os
+import sys
+import traceback
 
-# these file types will NOT be tracked as lfs, i.e. source code files:
-non_lfs_extensions = ['cs', 'txt', 'py', 'py', 'cpp', 'h', 'hpp', 'bat', 'sh', 'json', 'yaml', 'xml', 'strings', 'meta',
-                      'CS', 'TXT', 'PY', 'PY', 'CPP', 'H', 'HPP', 'BAT', 'SH', 'JSON', 'YAML', 'XML', 'STRINGS', 'META',
-                      'unity', 'prefab', 'controller', 'mat', 'renderTexture', 'flare', 'anim', 'asset',
-                      'gitignore', 'gitattributes']
+STATUS_CMD = ['git', 'status', '--porcelain']
+ADDED_FILE_FLAGS = ['A ', 'M ', '?? ']
+GIT_RESET_CMD = ['git', 'reset']
+GIT_ADD_CMD = ['git', 'add']
+STAGE_GITATTRS_CMD = ['git', 'add', '.gitattributes']
+GITATTRS_CHANGED = False
+DIVIDER = 80*'-'
 
-status_cmd = ['git', 'status', '--porcelain']
-added_file_flags = [' A ', 'A ', 'M ', ' M ', '??']
-git_reset_cmd = ['git', 'reset']
-git_add_cmd = ['git', 'add']
-added_changed_attributes_cmd = ['git', 'add', '.gitattributes']
-files_to_be_tracked_as_lfs = []
+# don't do anything, just print log:
+DRY_RUN = False
+
+
+def _get_non_lfs_extensions():
+    """
+    """
+    exts = []
+
+    non_lfs_files = os.path.join(os.getcwd(), 'git_hooks/non_lfs_files.txt')
+    
+    with open(non_lfs_files) as f:
+        exts = f.readlines()
+        exts = [x.strip() for x in exts]
+
+    return exts
 
 
 def track():
-    git_status = Popen(status_cmd, stdout=PIPE, stderr=PIPE)
+    """
+    Main entry point for hook, figure out what needs to be tracked as lfs and do it.
+    """
+    git_status = Popen(STATUS_CMD, stdout=PIPE, stderr=PIPE)
     git_status_result = git_status.stdout.readlines()
 
+    non_lfs_extensions = _get_non_lfs_extensions()
+    print '\nnon-lfs files: %s' % non_lfs_extensions
+    
     for line in git_status_result:
         line = line[:-1]
-        file_name_index = line.rfind(' ')
-        file_name = line[file_name_index+1:]
-        for file_flag in added_file_flags:
+        file_name = ''
 
-            if not line.startswith(file_flag):
-                continue
+        for file_flag in ADDED_FILE_FLAGS:
+            if line.startswith(file_flag):
+                file_name = line.replace(file_flag, '')
 
-            file_is_lfs = True
+        if file_name == '':
+            continue
+
+        if file_name.startswith(' '):
+            file_name = file_name[1::]
+
+        # remove any leading and trailing double quotes
+        if file_name.startswith('"') and file_name.endswith('"'):
+            file_name = file_name[1:-1:]
+        
+        file_is_lfs = True
+        for ext in non_lfs_extensions:
+            rgx = r'.*\.%s$' % ext
+            if re.match(rgx, file_name, re.IGNORECASE):
+                file_is_lfs = False
+
+        if file_is_lfs:
+
+            print '\n' + DIVIDER
+            print 'staged file: "%s"' % file_name
+            print 'file is lfs: %s' % file_is_lfs
             
-            for ext in non_lfs_extensions:
-                rgx = r'.*\.%s$' % ext
-                if re.match(rgx, file_name):
-                    file_is_lfs = False
-
-            print 'file_is_lfs:', file_is_lfs
-                    
-            if file_is_lfs:
-                try:
-                    _track_as_lfs(file_name)
-                except Exception as e:
-                    print "LFS tracking failed: "
-                    print e
+            try:
+                _track_as_lfs(file_name)
+                print "done"
+                print DIVIDER                
+            except Exception as e:
+                print "LFS tracking failed: "
+                print e
     
-    if 0 < len(files_to_be_tracked_as_lfs):
-        subprocess.check_call(added_changed_attributes_cmd)
+    if GITATTRS_CHANGED and not DRY_RUN:
+        print '\n.gitattributes were changed, staging:'
+        subprocess.check_call('git add .gitattributes', shell=True)
 
 
 def _track_as_lfs(file_name):
-    
+    """
+    Track a given file or file type (if extension is presnet) as lfs
+    """
+    # ensure file_name is wrapped in double quotes, for file paths with spaces
+    if not file_name.startswith('"') and not file_name.endswith('"'):
+        file_name = '"%s"' % file_name    
+
     file_split = file_name.split('.')
 
+    # track files with extensions as a wildcard:
     if len(file_split) > 1:
-        file_name = '"*.%s"' % file_split[-1]
+        lfs_pattern = '"*.%s' % file_split[-1]
 
-    print "LFS Tracking: " + file_name
-    files_to_be_tracked_as_lfs.append(file_name)
+    # track files without extensions as unique paths:
+    else:
+        lfs_pattern = file_name
 
-    reset_cmd = git_reset_cmd + [file_name]
-    lfs_cmd = 'git --work-tree "' + os.getcwd() + '" lfs track ' + file_name
-    add_cmd = git_add_cmd + [file_name]
+    reset_cmd = 'git reset ' + file_name
+    lfs_cmd = 'git --work-tree "' + os.getcwd() + '" lfs track ' + lfs_pattern
+    add_cmd = 'git add ' + file_name
 
-    subprocess.check_call(reset_cmd)
-    subprocess.check_call(lfs_cmd, shell=True)
-    subprocess.check_call(add_cmd)
+    print reset_cmd
+    print lfs_cmd
+    print add_cmd
+    
+    global GITATTRS_CHANGED
+    GITATTRS_CHANGED = True
+
+    if not DRY_RUN:
+        subprocess.check_call(reset_cmd, shell=True)
+        subprocess.check_call(lfs_cmd, shell=True)
+        subprocess.check_call(add_cmd, shell=True)
 
 
 if __name__ == "__main__":
@@ -76,9 +126,10 @@ if __name__ == "__main__":
     try:
         track()
     except Exception as e:
-        print "LFS tracking hook failed: "
-        print e
+        print "\nLFS tracking hook failed: "
+        exc_type, exc_value, exc_traceback = sys.exc_info()                
+        print traceback.format_exc()
         exit(1)
     
-    print "LFS tracking hook ended"
+    print "\nLFS tracking hook ended"
     exit(0)
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 Using Git for Unity3d projects requires usage of Git LFS (large file storage).  Files to be tracked as LFS can be defined using either paths or extensions, and this is stored in the .gitattributes file.
 
-However a problem with this is that it's not very artist friendly - git defaults to assuming no files are LFS, and the user has to opt-in to tell Git when a file is LFS.  Considering many CG artists are not familiar with version control, this is not idea.
+However a problem with this is that it's not very artist friendly - git defaults to assuming no files are LFS, and the user has to opt-in to tell Git when a file is LFS.  Considering many CG artists are not familiar with version control, this is not a good idea.
 
 This provides a git pre-commit hook, that tracks any file type not whitelisted as LFS.  This means artists cannot accidentally commit large media files as not-LFS, and the onus is on developers to whitelist any source code files they want to add.
 
